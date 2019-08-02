2112f565bbe781bea95ed60ee1d31ce4d7a0f987
Ryan <1264Ryan@qq.com>
2019-08-02 22:35:52 +0800

    表单组件样式和验证

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from '@/router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import router from '@/router'
+
 // 产品级别提示  上线阶段(true)  开发阶段(false)
 import '@/styles/index.less'
 Vue.config.productionTip = false
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,66 @@
 <template>
   <div class="container">
-    <el-card class="my-card"></el-card>
+    <el-card class="my-card">
+      <!-- logo -->
+      <img src="../../assets/images/logo_index.png" />
+      <!-- 表单 -->
+      <el-form :model="loginForm" :rules="loginRules" status-icon ref="loginForm">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:288px; margin-right:10px;"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%" @click="login">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    const checkMobile = (rule, value, callback) => {
+      if (!/^1[3-9]\d{9}$/.test(value)) return callback(new Error('格式不对'))
+      callback()
+    }
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位验证码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          console.log('success')
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'>
@@ -23,6 +78,11 @@ export default {}
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
+    img {
+      width: 200px;
+      display: block;
+      margin: 0 auto 30px;
+    }
   }
 }
 </style>
